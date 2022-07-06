8535dec5611f095b600a56e350cbe1a3e9b8a868
Charles Cheng <cowton0517@gmail.com>
2022-07-06 10:11:32 +0800

    Initial Commit

---
+++ ViewIfLoadedPrac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewIfLoadedPrac
+//
+//  Created by Chun-Li Cheng on 2022/7/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
